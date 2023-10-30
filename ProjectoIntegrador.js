b0ad8872b25b68c18333014b4a0444b5217458c7
MassimoLarger <massimoalberto.larger@alumnos.ulagos.cl>
2023-10-29 23:08:00 -0300

    Actualizacion

--- ProjectoIntegrador.js
+++ ProjectoIntegrador.js
@@ -120,7 +120,7 @@ function responderTrivia(botonSeleccionado) {
         // Fin del juego
         mostrarResultadoFinal();
       }
-    }, 100);  // Esperar 1 segundo (1000 milisegundos) antes de cargar la siguiente pregunta
+    }, 1000);  // Esperar 1 segundo (1000 milisegundos) antes de cargar la siguiente pregunta
   }
 }
 
